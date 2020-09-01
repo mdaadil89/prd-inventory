a18c68cd7593cbfb0028dd5eba14cf239439ca74
Md Aadil <mdaadilhus9@gmail.com>
2020-09-01 22:45:22 +0530

    Minor Modification

--- src/components/about/about.component.js
+++ src/components/about/about.component.js
@@ -3,7 +3,7 @@ import './about.styles.css'
 
 
 const About = () =>   (
- <div>
+ <div className='headerbody'>
      <h3>PRODUCT INVENTORY</h3>
      <p>
      This system is provisioned to view the list of products and allow only the registered and authenticated users to add, edit, delete the products and view the product detail. The user interface to the system has been implemented using React. The data about the products is managed in a JSON server. The client application interfaces with the JSON Server through http to retrieve and store data.
